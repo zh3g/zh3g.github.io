3a0e85288139e35917f6847be508bfe421e9d3a4
zh3g <zhang1zhengzheng@outlook.com>
2023-07-10 07:58:18 +0800

    Site updated: 2023-07-10 07:58:10

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pwa: {
+        iconPaths: {
+            favicon32: 'favicon3.ico',
+            favicon16: 'favicon3.ico',
+            appleTouchIcon: 'favicon3.ico',
+            maskIcon: 'favicon3.ico',
+            msTileImage: 'favicon3.ico'
+        }
+    }
+}
+
